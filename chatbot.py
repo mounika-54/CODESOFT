4529662e64cf073e91d45cb9612df3fe9b876695
DANNAVARAPU MOUNIKA <mounikadannavarapu@gmail.com>
2026-06-30 15:28:28 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,25 @@
 # CODESOFT
+print("🤖 Welcome to Simple Chatbot!")
+print("Type 'bye' to exit.\n")
+
+while True:
+    user = input("You: ").lower()
+
+    if user == "hi" or user == "hello":
+        print("Bot: Hello! How are you?")
+
+    elif user == "how are you":
+        print("Bot: I am fine. Thank you!")
+
+    elif user == "what is your name":
+        print("Bot: My name is SimpleBot.")
+
+    elif user == "who created you":
+        print("Bot: I was created using Python.")
+
+    elif user == "bye":
+        print("Bot: Goodbye! Have a nice day.")
+        break
+
+    else:
+        print("Bot: Sorry, I don't understand.")
